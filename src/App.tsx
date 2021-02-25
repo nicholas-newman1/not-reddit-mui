c8e91932c2fa131485be6cb03a16f675c409b369
Nicholas Newman <nicholas.newman1@hotmail.com>
2021-02-25 15:19:16 -0500

    created header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
 import Home from './pages/Home';
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path='/' component={Home} />
-      </Switch>
+      <Header />
+      <main>
+        <Switch>
+          <Route path='/' component={Home} />
+        </Switch>
+      </main>
     </Router>
   );
 }
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,51 @@
+import { useEffect, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
+import Menu from '../../svg/Menu';
+import styles from './Header.module.scss';
+
+const Header = () => {
+  const [displayMenu, setDisplayMenu] = useState(false);
+  const header = useRef<HTMLElement>(null);
+
+  const toggleDisplayMenu = () => {
+    setDisplayMenu((prev) => !prev);
+  };
+
+  useEffect(() => {
+    /* clicking on main closes the menu */
+    const main = document.querySelector('main')!;
+    main.addEventListener('click', () => setDisplayMenu(false));
+
+    /* Header animates */
+    header.current!.classList.add(styles.headerAnimate);
+  }, []);
+
+  return (
+    <header ref={header} className={styles.header}>
+      <div className={styles.container}>
+        <Link to='/' className={styles.logo}>
+          Not Reddit
+        </Link>
+
+        <div className={styles.hamburger} onClick={toggleDisplayMenu}>
+          <Menu />
+        </div>
+
+        <div
+          className={`${styles.menu} ${displayMenu ? styles.menuActive : ''}`}
+        >
+          <div className={styles.login}>
+            <button className={styles.loginBtn + ' btn btn-outline-white'}>
+              Log In
+            </button>
+            <button className={styles.signUpBtn + ' btn btn-white'}>
+              Sign Up
+            </button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
