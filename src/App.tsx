f4dff0ee198973ef2b65e928328a1759a66ba111
Nicholas Newman <nicholas.newman1@hotmail.com>
2021-03-09 07:35:55 -0500

    added material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
+import { CssBaseline, withStyles } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/styles';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
 import Header from './components/Header';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
-import { light, dark } from './theme';
+import { theme } from './theme';
 
 function App() {
   return (
-    <ThemeProvider theme={dark}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <Router>
         <Header />
         <main>
--- src/theme.ts
+++ src/theme.ts
@@ -1,44 +1,29 @@
-const theme = {
-  colors: {
-    primary: {
-      main: '#008aa6',
-      text: '#fff',
-    },
-    secondary: {
-      main: '#c28800',
-      text: '#fff',
-    },
-  },
-  transitions: {
-    duration: {
-      standard: '200ms',
-    },
-  },
-  borderRadius: '5px',
-  zIndex: {
-    modal: 200,
-    nav: 100,
-  },
-  shadow: '5px 5px 5px rgba(0, 0, 0, 0.15)',
-  hoverOpacity: 0.8,
-};
+import { createMuiTheme } from '@material-ui/core';
 
-export const light = {
-  ...theme,
-  colors: {
-    ...theme.colors,
-    background: '#fff',
-    card: '#e1e1e1',
-    text: '#000',
-  },
-};
+function getTheme(theme: { paletteType: string }) {
+  return createMuiTheme({
+    palette: {
+      type: 'light',
+      primary: {
+        main: '#008aa6',
+      },
+      secondary: {
+        main: '#c28800',
+      },
+      background: {
+        paper: theme.paletteType === 'dark' ? '#333' : '#e1e1e1',
+        default: theme.paletteType === 'dark' ? '#1a1a1a' : '#fff',
+      },
+    },
+  });
+}
 
-export const dark = {
-  ...theme,
-  colors: {
-    ...theme.colors,
-    background: '#1a1a1a',
-    card: '#333',
-    text: '#eee',
+export const theme = getTheme({
+  paletteType: 'dark',
+});
+
+theme.props = {
+  MuiCard: {
+    elevation: 0,
   },
 };
