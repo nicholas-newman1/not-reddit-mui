7a6c32f801fcca1be4ce06ff6a9411d26f63b886
Nicholas Newman <nicholas.newman1@hotmail.com>
2021-03-26 09:52:36 -0400

    setup firebase locally

--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -2,7 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 
-const app = firebase.initializeApp({
+let config: any = {
   apiKey: 'AIzaSyDUBhjnoHZOSxsp69GMHL6XfiBeSWC1-44',
   authDomain: 'not-reddit-5a7e3.firebaseapp.com',
   projectId: 'not-reddit-5a7e3',
@@ -10,10 +10,19 @@ const app = firebase.initializeApp({
   messagingSenderId: '951984622978',
   appId: '1:951984622978:web:7e88776937ac9d4836139b',
   measurementId: 'G-2YSX4475S9',
-});
+};
+
+if (process.env.NODE_ENV === 'development') {
+  config.databaseURL = 'http://localhost:8080?ns=emulatorui';
+}
+
+const app = firebase.initializeApp(config);
 
 export type FirebaseError = firebase.FirebaseError;
 export type FirebaseUser = firebase.User | null;
+export const auth = firebase.auth();
 export const db = firebase.firestore(app);
-export const auth = app.auth();
-export default app;
+
+if (process.env.NODE_ENV === 'development') {
+  auth.useEmulator('http://localhost:9099');
+}
