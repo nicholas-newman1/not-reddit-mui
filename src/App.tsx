dafb746d3cf45496aef63b0746b332bfe375698c
Nicholas Newman <nicholas.newman1@hotmail.com>
2021-04-13 07:05:29 -0400

    created <Singletons />

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,20 @@ import { CssBaseline } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/styles';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
-import ResetPasswordDialogContainer from './containers/ResetPasswordDialogContainer';
-import ResetPasswordSentToastContainer from './containers/ResetPasswordSentToastContainer';
-import SentEmailVerificationDialogContainer from './containers/SentEmailVerificationDialogContainer';
-import SignInDialogContainer from './containers/SignInDialogContainer';
-import SignUpDialogContainer from './containers/SignUpDialogContainer';
-import SignUpSuccessToastContainer from './containers/SignUpSuccessToastContainer';
+import Singletons from './components/Singletons';
 import Category from './pages/Category';
 import Home from './pages/Home';
 import { theme } from './theme';
 
-function App() {
+const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <SignInDialogContainer />
-      <SignUpDialogContainer />
-      <ResetPasswordDialogContainer />
-      <SignUpSuccessToastContainer />
-      <ResetPasswordSentToastContainer />
-      <SentEmailVerificationDialogContainer />
 
       <Router>
         <Header />
         <main>
+          <Route path='/' component={Singletons} />
           <Switch>
             <Route exact path='/' component={Home} />
             <Route exact path='/categories/:categoryId' component={Category} />
@@ -34,6 +24,6 @@ function App() {
       </Router>
     </ThemeProvider>
   );
-}
+};
 
 export default App;
---
+++ src/components/Singletons/index.tsx
@@ -0,0 +1,25 @@
+import CreatePostDialogContainer from '../../containers/CreatePostDialogContainer';
+import CreatePostSuccessToastContainer from '../../containers/CreatePostSuccessToastContainer';
+import ResetPasswordDialogContainer from '../../containers/ResetPasswordDialogContainer';
+import ResetPasswordSentToastContainer from '../../containers/ResetPasswordSentToastContainer';
+import SentEmailVerificationDialogContainer from '../../containers/SentEmailVerificationDialogContainer';
+import SignInDialogContainer from '../../containers/SignInDialogContainer';
+import SignUpDialogContainer from '../../containers/SignUpDialogContainer';
+import SignUpSuccessToastContainer from '../../containers/SignUpSuccessToastContainer';
+
+const Singletons = () => {
+  return (
+    <>
+      <SignInDialogContainer />
+      <SignUpDialogContainer />
+      <ResetPasswordDialogContainer />
+      <SignUpSuccessToastContainer />
+      <ResetPasswordSentToastContainer />
+      <SentEmailVerificationDialogContainer />
+      <CreatePostDialogContainer />
+      <CreatePostSuccessToastContainer />
+    </>
+  );
+};
+
+export default Singletons;
