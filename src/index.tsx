c210d3a0b60e11c2f26eb29652fac9b690283349
nick <nicholas.newman1@hotmail.com>
2021-05-10 06:53:51 -0400

    removed redux-persist

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
-import { store, persistor } from './store';
-import { PersistGate } from 'redux-persist/integration/react';
+import store from './store';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
+      <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,19 +1,4 @@
-import {
-  combineReducers,
-  configureStore,
-  getDefaultMiddleware,
-} from '@reduxjs/toolkit';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import { configureStore } from '@reduxjs/toolkit';
 import auth, { verifyAuth } from './authSlice';
 import createCategory from './createCategorySlice';
 import createPost from './createPostSlice';
@@ -24,47 +9,24 @@ import ratingStatus from './ratingStatusSlice';
 import categoryMeta from './categoryMetaSlice';
 import postPage from './postPageSlice';
 
-const rootReducer = combineReducers({
-  auth,
-  createCategory,
-  createPost,
-  categoryPage,
-  homePage,
-  subscribedCategories,
-  ratingStatus,
-  categoryMeta,
-  postPage,
+const store = configureStore({
+  reducer: {
+    auth,
+    createCategory,
+    createPost,
+    categoryPage,
+    homePage,
+    subscribedCategories,
+    ratingStatus,
+    categoryMeta,
+    postPage,
+  },
 });
 
-const persistConfig = {
-  key: 'root',
-  storage,
-  whitelist: [
-    'auth',
-    'createCategory',
-    'createPost',
-    'categoryPage',
-    'homePage',
-    'subscribedCategories',
-    'ratingStatus',
-    'categoryMeta',
-    'postPage',
-  ],
-};
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: getDefaultMiddleware({
-    serializableCheck: {
-      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-    },
-  }),
-});
-export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
 //@ts-ignore
 store.dispatch(verifyAuth());
+
+export default store;
